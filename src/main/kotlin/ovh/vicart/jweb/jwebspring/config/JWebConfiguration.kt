a6388bd3d5ca317d8cc3b2d95f07e446d00cfd93
Vicart Clément <clem.vicart@gmail.com>
2021-02-21 19:20:05 +0100

    first commit

---
+++ src/main/kotlin/ovh/vicart/jweb/jwebspring/config/JWebConfiguration.kt
@@ -0,0 +1,18 @@
+package ovh.vicart.jweb.jwebspring.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Configuration
+import ovh.vicart.jweb.JWeb
+
+@Configuration
+abstract class JWebConfiguration {
+
+    @Value("\${server.servlet.context-path}")
+    private var context: String? = null
+
+    init {
+        JWeb.configure().withContextPath(if(context != null) context!! else "/")
+    }
+}
---
+++ src/main/kotlin/ovh/vicart/jweb/jwebspring/web/SpringWebContentURL.kt
@@ -0,0 +1,14 @@
+package ovh.vicart.jweb.jwebspring.web
+
+import ovh.vicart.jweb.web.WebContentURL
+import java.net.URL
+import javax.servlet.http.HttpServletRequest
+
+class SpringWebContentURL {
+
+    companion object {
+        fun getWebContentFrom(localContent: String, request: HttpServletRequest) : URL {
+            return WebContentURL.getWebContentFrom(localContent, URL(request.requestURL.toString()))
+        }
+    }
+}
